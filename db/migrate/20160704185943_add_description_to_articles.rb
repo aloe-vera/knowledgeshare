2b431dbb0263c7eb4f587ea0588278d279a7edba
tocala <mariusjora@gmail.com>
2016-07-04 20:23:39 +0000

    added table to DB and created a model with a few validations

---
+++ db/migrate/20160704185943_add_description_to_articles.rb
@@ -0,0 +1,8 @@
+class AddDescriptionToArticles < ActiveRecord::Migration
+  def change
+    add_column :posts, :description, :text
+    add_column :posts, :updated_at, :datetime
+    add_column :posts, :created_at, :datetime
+
+  end
+end
